edb0c687836ed27aaf295e1c5ea281a79e0f5622
Greg Rayburn <greg.rayburn@gmail.com>
2017-06-02 15:25:52 -0700

    Create ProbeReqRSSI-2.py
    Sniff client probe request frames and print Mac address + RSSI + SSID and append to list

---
+++ scapy scripts/ProbeReqRSSI-2.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+client_list = []
+def PacketHandler(pkt) :
+    if pkt.haslayer(Dot11) :
+        if pkt.type == 0 and pkt.subtype == 4 :
+            if pkt.haslayer(Dot11ProbeReq):
+                try:
+                    extra = pkt.notdecoded
+                    rssi = -(256-ord(extra[-2:-1]))
+                except:
+                    rssi = -100
+                if pkt.addr2 not in client_list :
+                    client_list.append(pkt.addr2)
+                    print "WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info
+sniff(iface="wlan0mon", prn = PacketHandler)
